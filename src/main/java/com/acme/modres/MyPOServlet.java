ec37bb72aafd3d7e057c2cdd1486a6c4d221c952
Fabio Franco <EASeJ.Tools@ibm.com>
2025-10-23 17:13:18 -0400

    Fix warning

--- src/main/java/com/acme/modres/MyPOServlet.java
+++ src/main/java/com/acme/modres/MyPOServlet.java
@@ -1,9 +1,9 @@
 package com.acme.modres;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+// import java.io.PrintWriter;
 
-import javax.inject.Inject;
+// import javax.inject.Inject;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
